7aa0e23e452458476b67125b0bcab2d970b9b07c
jestiii <jestinethomas556@gmail.com>
2026-02-06 18:26:44 +0530

    added

--- src/CountCharacters.java
+++ src/CountCharacters.java
@@ -1,10 +1,33 @@
+import java.util.*;
 public class CountCharacters {
-    
-        // TODO: Read the string line
-        // TODO: Initialize counters for vowels, consonants, digits, special
-        // TODO: Convert to lowercase for easier checking (optional but recommended)
-        // TODO: Iterate through characters and update counters
-        // Hint: Character.isDigit(ch), Character.isLetter(ch) might be useful
-        // TODO: Print the 4 counts separated by spaces
-    
+        public static void main(String[] args) {
+                Scanner sc=new Scanner(System.in);
+                String s=sc.nextLine();
+                int vowel=0;
+                int consonant=0;
+                int digit=0;
+                int special=0;
+                s=s.toLowerCase();
+                int n=s.length();
+                for(int i=0;i<n;i++){
+                        char ch=s.charAt(i);
+                        if(Character.isLetter(ch)){
+                                ch=Character.toLowerCase(ch);
+                                if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+                                        vowel++;
+                                }
+                                else{
+                                        consonant++;
+                                }
+                        }
+                        else if(Character.isDigit(ch)){
+                                digit++;
+                        }
+                        else{
+                                special++;
+                        }
+                }
+                System.out.println(vowel + " " + consonant + " " + digit + " " + special);
+        }
+        
 }
